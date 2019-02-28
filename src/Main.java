741dcafd995c1b905d1ff93f21280c2173c6b3d8
RossFY <yfang14@stevens.edu>
2019-02-27 23:39:29 -0500

    Updated

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 // This is the Main class
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		int choose = 0;
 	}
 
 }
