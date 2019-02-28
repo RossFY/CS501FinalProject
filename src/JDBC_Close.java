8ee20d9a8b146e6d26054a3e3f1bcbfdf7fd135d
RossFY <yfang14@stevens.edu>
2019-02-28 13:47:19 -0500

    Updated

--- src/JDBC_Close.java
+++ src/JDBC_Close.java
@@ -4,6 +4,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class JDBC_Close {
+	
 	public static void close(ResultSet resultSet) {
         try {
             if (resultSet != null) {
